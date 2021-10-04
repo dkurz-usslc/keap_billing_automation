8bec83b30834bdbadcbcb80bf48a83646086fc1e
dkurz-usslc <dkurz@usslc.org>
2021-10-04 14:33:55 -0400

    Initial commit

---
+++ expiring_cards.py
@@ -0,0 +1,47 @@
+from keap import xml, rest, dir_path, helpers
+import datetime
+import pandas as pd
+import os
+
+
+def get_current_month_year():
+    today_date = datetime.date.today()
+    print(today_date)
+    today_year = datetime.date.strftime(today_date, '%Y')
+    print(today_year)
+    today_month = datetime.date.strftime(today_date, '%m')
+    print(today_month)
+    find_expiring_cards(today_month, today_year)
+
+
+def find_expiring_cards(exp_month, exp_year):
+    cards = xml.Query(app='ed153',
+                      query_search={'ExpirationMonth': exp_month, 'ExpirationYear': exp_year},
+                      table='CreditCard',
+                      return_fields=['Id', 'ContactId', 'Last4', 'ExpirationMonth', 'ExpirationYear', 'Status']).query()
+    cards_df = pd.DataFrame(data=cards,
+                            columns=['card_id', 'contact_id', 'card_last_four', 'exp_month', 'exp_year', 'status'])
+    print(cards_df)
+
+
+def remove_canceled_contacts(app):
+    app_path = os.path.join(dir_path, app)
+    canceled_list = rest.ListContactsWithTag(path=app_path, tag_id=493).rest_call()
+    print(canceled_list["next"])
+    helpers.flatten_sorter(canceled_list)
+
+
+def today_order_check(app):
+    pd.set_option('display.max_columns', None)
+    pd.options.display.width = 0
+    app_path = os.path.join(dir_path, app)
+    today = datetime.datetime.today()
+    today_str = today.strftime("%Y-%m-%dT00:00:00.000Z")
+    resp = rest.ListOrders(app_path, {'since': today_str, 'paid': False}).rest_call()
+    print(resp)
+    helpers.flatten_sorter(resp)
+
+
+# get_current_month_year()
+remove_canceled_contacts('ed153')
+# today_order_check('ed153')
