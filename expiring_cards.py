3be0d7b872d27dc6b7c14170345c77fab1772797
dkurz-usslc <dkurz@usslc.org>
2021-10-19 08:39:55 -0400

    fully takes advantage of multithreading for cancel list

--- expiring_cards.py
+++ expiring_cards.py
@@ -1,47 +1,59 @@
 from keap import xml, rest, dir_path, helpers
 import datetime
 import pandas as pd
+import timeit
 import os
 
 
-def get_current_month_year():
+def get_current_month_year(app):
     today_date = datetime.date.today()
     print(today_date)
     today_year = datetime.date.strftime(today_date, '%Y')
     print(today_year)
     today_month = datetime.date.strftime(today_date, '%m')
     print(today_month)
-    find_expiring_cards(today_month, today_year)
+    find_expiring_cards(today_month, today_year, app)
 
 
-def find_expiring_cards(exp_month, exp_year):
-    cards = xml.Query(app='ed153',
-                      query_search={'ExpirationMonth': exp_month, 'ExpirationYear': exp_year},
-                      table='CreditCard',
-                      return_fields=['Id', 'ContactId', 'Last4', 'ExpirationMonth', 'ExpirationYear', 'Status']).query()
+def find_expiring_cards(exp_month, exp_year, app):
+    cards = helpers.query_builder(app=app, table='CreditCard',
+                                  return_fields=['Id', 'ContactId', 'Last4', 'ExpirationMonth', 'ExpirationYear',
+                                                 'Status'], ExpirationMonth=exp_month, ExpirationYear=exp_year)
     cards_df = pd.DataFrame(data=cards,
                             columns=['card_id', 'contact_id', 'card_last_four', 'exp_month', 'exp_year', 'status'])
     print(cards_df)
+    remove_canceled_contacts(app, cards_df)
 
 
-def remove_canceled_contacts(app):
-    app_path = os.path.join(dir_path, app)
-    canceled_list = rest.ListContactsWithTag(path=app_path, tag_id=493).rest_call()
-    print(canceled_list["next"])
-    helpers.flatten_sorter(canceled_list)
+# def today_order_check(app):
+#     pd.set_option('display.max_columns', None)
+#     pd.options.display.width = 0
+#     app_path = os.path.join(dir_path, app)
+#     today = datetime.datetime.today()
+#     today_str = today.strftime("%Y-%m-%dT00:00:00.000Z")
+#     resp = rest.ListOrders(app_path, {'since': today_str, 'paid': False}).rest_call()
+#     remove_canceled_contacts(app, resp)
 
 
-def today_order_check(app):
-    pd.set_option('display.max_columns', None)
-    pd.options.display.width = 0
-    app_path = os.path.join(dir_path, app)
-    today = datetime.datetime.today()
-    today_str = today.strftime("%Y-%m-%dT00:00:00.000Z")
-    resp = rest.ListOrders(app_path, {'since': today_str, 'paid': False}).rest_call()
-    print(resp)
-    helpers.flatten_sorter(resp)
+def remove_canceled_contacts(app, data):
+    cancel_set = helpers.get_set_of_canceled_contacts(app)
+    card_set = set(data["contact_id"])
+    print(card_set)
+    not_canceled_card_set = card_set.difference(cancel_set)
+    print(not_canceled_card_set)
+    not_canceled_card_df = data[data["contact_id"].isin(not_canceled_card_set)]
+    print(not_canceled_card_df)
 
 
-# get_current_month_year()
-remove_canceled_contacts('ed153')
+def find_time(app):
+    l = helpers.get_set_of_canceled_contacts(app)
+    n_l = helpers.test_get_set_of_canceled_contacts(app)
+    print(len(l))
+    print(len(n_l))
+    print(l == n_l)
+
+
+find_time('ed153')
+# get_current_month_year("ed153")
+# remove_canceled_contacts('ed153')
 # today_order_check('ed153')
